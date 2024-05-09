fa9e041b6143665bb710bcc61ad8be4073e59435
javid shabin <javid.main.act@gmail.com>
2024-05-09 21:16:10 +0530

    add a video the html

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 let playBtn = document.getElementById("play-btn");
 let video = document.querySelector(".video");
 
-
+let isVideoPlaying = false;
 
 const playOrPuseVideo = () => {
-
+    
 }
 
-playBtn.addEventListener("click", playOrPuseVideo)
+playBtn.addEventListener("click", playOrPuseVideo);
