0fc2026bc77a43e65f58c7026b14512878fb7f8b
javid shabin <javid.main.act@gmail.com>
2024-05-09 21:14:42 +0530

    add video in the project

---
+++ script.js
@@ -0,0 +1,10 @@
+let playBtn = document.getElementById("play-btn");
+let video = document.querySelector(".video");
+
+
+
+const playOrPuseVideo = () => {
+
+}
+
+playBtn.addEventListener("click", playOrPuseVideo)
