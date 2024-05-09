2e4009b1351fcefaf4f2423da312268f231ebb45
javid shabin <javid.main.act@gmail.com>
2024-05-09 21:53:14 +0530

    add progress bar

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let playBtn = document.getElementById("play-btn");
 let video = document.querySelector(".video");
+let progressBar = document.querySelector(".progress-bar")
+console.log(progressBar);
 
 let isVideoPlaying = false;
 
@@ -11,9 +13,17 @@ const playOrPuseVideo = () => {
     }else {
         video.pause()
         isVideoPlaying = false;
-        playBtn.classList.replace("fa-pause", "fa-play")
+        playBtn.classList.replace("fa-pause", "fa-play");
     }
 }
 
+const updateProgressbar = (event) => {
+    console.log(event.target.currentTime, event.target.duration);
+    progressBar.style.cssText = `
+        width: ${event.target.currentTime}%
+    `
+}
+
 playBtn.addEventListener("click", playOrPuseVideo);
-video.addEventListener("click", playOrPuseVideo)
+video.addEventListener("click", playOrPuseVideo);
+video.addEventListener("timeupdate", updateProgressbar)
