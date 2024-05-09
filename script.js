a868c2f77d50bb75fed75c30086a1d934a1743b5
javid shabin <javid.main.act@gmail.com>
2024-05-09 21:21:04 +0530

    video play or pause function perfectly added

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 let playBtn = document.getElementById("play-btn");
 let video = document.querySelector(".video");
+let videoBtn = document.getElementById("play-btn");
 
 let isVideoPlaying = false;
 
 const playOrPuseVideo = () => {
-    
+    if (!isVideoPlaying) {
+        video.play()
+        isVideoPlaying = true;
+
+    }else {
+        video.pause()
+        isVideoPlaying = false;
+    }
 }
 
 playBtn.addEventListener("click", playOrPuseVideo);
+video.addEventListener("click", playOrPuseVideo)
